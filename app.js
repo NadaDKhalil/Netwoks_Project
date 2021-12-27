19066f7c1ac1443e712201b48c313c9a6e2dd20d
Ragaa Aly <ragaa.aly20@gmail.com>
2021-12-28 00:24:05 +0200

    Add To cart

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var jsdom = require("jsdom");
-var JSDOM = jsdom.JSDOM;
-global.document = new JSDOM("html").window.document;
-//var popupS = require('popups');
 let alert = require('alert');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const req = require('express/lib/request');
+
+var { MongoClient } = require('mongodb');
+var uri = "mongodb+srv://admin:admin@cluster0.9mj9q.mongodb.net/firstdb?retryWrites=true&w=majority"
+var client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +17,11 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var sess;
 //GET requests
 app.get('/', function (req, res) {
     res.render('login')
@@ -71,34 +79,84 @@ app.get('/tennis', function (req, res) {
     res.render('tennis')
 });
 //POST requests
+app.post('/addgalaxy', function (req, res) {
+    var product = "galaxy";
+    addToCart(product);
+});
+app.post('/addiphone', function (req, res) {
+    var product = "iphone";
+    addToCart(product);
+});
+app.post('/addleaves', function (req, res) {
+    var product = "leaves";
+    addToCart(product);
+});
+app.post('/addsun', function (req, res) {
+    var product = "sun";
+    addToCart(product);
+});
+app.post('/addtennis', function (req, res) {
+    var product = "tennis";
+    addToCart(product);
+});
+app.post('/addboxing', function (req, res) {
+    var product = "boxing";
+    addToCart(product);
+});
+async function addToCart(product) {
+    await client.connect();
+    var username = { username: sess.username };
+    var user = await client.db('firstdb').collection('firstcollection').findOne(username);
+    console.log(user);
+    var cart = user.cart;
+    var found = false;
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i] == product) {
+            found = true;
+        }
+    }
+    if (found) {
+        alert("Product is already in your cart!");
+    }
+    else {
+        alert("Product is added successfully!")
+        cart.push(product);
+        var newcart = { $set: { cart: cart } };
+        await client.db('firstdb').collection('firstcollection').updateOne(username, newcart, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+        });
+    }
+    //client.close();
+}
+app.post('/cart', function (req, res) {
+    //res.render('cart',{ product: , pages:  });
+});
 //registration
 app.post('/register', function (req, res) {
     var x = req.body.username;
     var y = req.body.password;
-    var user = { username: x, password: y };
+    var user = { username: x, password: y, cart: [] };
     registration(user, res).catch(console.error);
 });
 //login
 app.post('/', function (req, res) {
     var x = req.body.username;
     var y = req.body.password;
+    sess = req.session
+    sess.username = x;
     var user = { username: x, password: y };
     login(user, res).catch(console.error);
 });
 //search
 app.post('/search', function (req, res) {
     var x = req.body.Search;
-
     search(x, res).catch(console.error);
-    //console.log(x);
-    // res.render('searchresults');
 });
 //
 async function search(x, res) {
-    var { MongoClient } = require('mongodb');
-    var uri = "mongodb+srv://admin:admin@cluster0.9mj9q.mongodb.net/firstdb?retryWrites=true&w=majority"
-    var client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
+
     var result = []
     var resultPages = []
     var out = ["Boxing Bag", "Galaxy S21 Ultra", "iPhone 13 Pro", "Leaves of Grass", "The Sun and Her Flowers", "Tennis Racket"];
@@ -113,7 +171,7 @@ async function search(x, res) {
         console.log();
     }
     console.log(result);
-    if (result.length == 0) {
+    if (result.length == 0 || x == "") {
         alert("Item not found")
     }
     else {
@@ -123,28 +181,26 @@ async function search(x, res) {
 }
 
 async function registration(x, res) {
-    var { MongoClient } = require('mongodb');
-    var uri = "mongodb+srv://admin:admin@cluster0.9mj9q.mongodb.net/firstdb?retryWrites=true&w=majority"
-    var client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
-
     var out = await client.db('firstdb').collection('firstcollection').find({ username: x.username }).toArray();
     console.log(out);
     if (out.length != 0) {
         alert("Username is already used!")
         res.render('registration');
     }
+    else if (x.username == "" || x.password == "") {
+        alert("Username and Password can not be empty!");
+        res.redirect('registration');
+    }
     else {
         await client.db('firstdb').collection('firstcollection').insertOne(x);
-        res.render('login');
+        alert("Registration was successful!");
+        res.redirect('/');
     }
     client.close();
 }
 
 async function login(x, res) {
-    var { MongoClient } = require('mongodb');
-    var uri = "mongodb+srv://admin:admin@cluster0.9mj9q.mongodb.net/firstdb?retryWrites=true&w=majority"
-    var client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     var out = await client.db('firstdb').collection('firstcollection').find({ username: x.username, password: x.password }).toArray();
     console.log(out);
@@ -159,7 +215,3 @@ async function login(x, res) {
 }
 //main().catch(console.error);
 app.listen(3000);
-/*
-display:flex;
-flex-wrap: wrap;
-*/ 
